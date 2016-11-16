8976dad3549b29b50670016b95431f55ee981f19
Dylan Evans <dylan@dje.me>
2016-11-16 11:08:38 +1000

    Renamed static function getline to imc_getline to remove conflict error

--- Dev/src/imc.c
+++ Dev/src/imc.c
@@ -633,7 +633,7 @@ static void do_send(imc_connect *c, const char *line)
 /*  try to read a line from the input buffer, NULL if none ready
  *  all lines are \n\r terminated in theory, but take other combinations
  */
-static const char *getline(char *buffer)
+static const char *imc_getline(char *buffer)
 {
   int i;
   char *buf=imc_getsbuf(IMC_PACKET_LENGTH);
@@ -1663,7 +1663,7 @@ void imc_idle_select(fd_set *read, fd_set *write, fd_set *exc, time_t now)
     while (c->state!=IMC_CLOSED &&
 //	   (c->spamtime1>=0 || c->spamcounter1<=IMC_SPAM1MAX) &&
 //	   (c->spamtime2>=0 || c->spamcounter2<=IMC_SPAM2MAX) &&
-	   (command = getline(c->inbuf)) != NULL)
+	   (command = imc_getline(c->inbuf)) != NULL)
     {
       if (strlen(command) > imc_stats.max_pkt)
 	imc_stats.max_pkt=strlen(command);
